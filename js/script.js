46c396971baa0a0e6072260034d68028266ac223
Lauren <laurenortencio@gmail.com>
2015-09-24 14:34:57 -0700

    first item in gallery links to last item when << is pressed; same, conversely

--- js/script.js
+++ js/script.js
@@ -1,60 +1,78 @@
+var NUM_IMAGES = 30;
+
+document.addEventListener('DOMContentLoaded', loadGallery());
+
+//adds script to body
+function loadGallery() {
+    var gallery = document.getElementById("gallery");    
+    var tag = "norwegianforestcat";
+    var pics = document.createElement("script");
+    //note: access token only shown here for purpose of coding challenge; in production,
+    //API calls would be made on server side and token would be stored in an environment variable.
+    pics.src = "https://api.instagram.com/v1/tags/" + tag + "/media/recent?callback=processInstaData&count=" + NUM_IMAGES + "&access_token=16384709.6ac06b4.49b97800d7fd4ac799a2c889f50f2587";
+    document.body.appendChild(pics);
+    document.body.removeChild(pics);
+}
+
 //gets data from instagram server
-function processInstaData(obj) {
-  console.log(obj);
-  for (var i=0; i<obj['data'].length; i++) {
-    thumb_url = obj['data'][i]['images']['thumbnail']['url'];
-    standard_url = obj['data'][i]['images']['standard_resolution']['url'];
-    caption = obj['data'][i]['caption']['text'];
-    link = obj['data'][i]['link'];
-    // console.log(caption);
-    img_id = i;
-    prev = i-1;
-    next = i+1;
-    // selectedBreed();
-    addImages(thumb_url, img_id);
-    addLightboxHTML(standard_url, img_id, caption, link, prev, next);
+function processInstaData(instagramData) {
+    var images = instagramData['data'];
+    for (var i=0; i<images.length; i++) {
+        var pic = images[i];
+        var thumbUrl = pic.images.thumbnail.url;
+        var standardUrl = pic.images.standard_resolution.url;
+        var caption = pic.caption.text;
+        var link = pic.link;
+        var imgId = i;
+        // var prev = i-1;
+        // var next = i+1;
+        if (imgId == 0) {
+            prev = 29;
+        }
+        else {
+            prev = i-1;
+        }
+        if (imgId == 29) {
+            next = 0;
+        }
+        else {
+            next = i+1;
+        }
+        addImages(thumbUrl, imgId);
+        addLightboxHTML(standardUrl, imgId, caption, link, prev, next);
   }
 }
 
-//adds script to body
-var tag = "fluffycat";
-var pics = document.createElement('script');
-pics.src = "https://api.instagram.com/v1/tags/" + tag + "/media/recent?callback=processInstaData&count=30&access_token=16384709.6ac06b4.49b97800d7fd4ac799a2c889f50f2587";
-document.body.appendChild(pics);
-
-function selectedBreed(evt) {
+function selectedBreed(form) {
+    console.log(form);
     var tag = document.getElementById("tag").value;
-    console.log(tag);
     setTag(tag);
-    evt.preventDefault();
 }
 
 function setTag(tag) {
     document.getElementById('gallery').innerHTML = "";
+    document.getElementById('lightbox').innerHTML = "";
     var pics = document.createElement('script');
     //note: access token only shown here for purpose of coding challenge; in production,
     //API calls would be made on server side and token would be stored in an environment variable.
     pics.src = "https://api.instagram.com/v1/tags/" + tag + "/media/recent?callback=processInstaData&count=30&access_token=16384709.6ac06b4.49b97800d7fd4ac799a2c889f50f2587";
-document.body.appendChild(pics);
+    document.body.appendChild(pics);
+    document.body.removeChild(pics);
 }
 
 //adds images to #gallery div
-function addImages(thumb_url, img_id) {
+function addImages(thumbUrl, imgId) {
     var img = document.createElement('img');
-    img.src = thumb_url;
+    img.src = thumbUrl;
     gallery = document.getElementById('gallery');
-    gallery.innerHTML += '<a href="#' + img_id + '"><img src="' + thumb_url + '"></a>';
+    gallery.innerHTML += '<a href="#' + imgId + '"><img src="' + thumbUrl + '"></a>';
 }
 
-function addLightboxHTML(standard_url, img_id, caption, link, prev, next) {
+function addLightboxHTML(standardUrl, imgId, caption, link, prev, next) {
     // trims long captions to 140 characters
-    capt = '';
     if (caption.length > 140 ) {
-        for (var i=0; i<140; i++) {
-        capt += caption[i];
-        }
-        capt += "...";
+        caption = caption.slice(0,140) + '...';
     }
 
-    lightbox.innerHTML += '<span class="lightbox" id="' + img_id + '"><a href="#_"><div class="close">close x</div></a><div class="prevnext"><a href="#' + prev + '" class="left"><< previous</a><a href="#' + next + '" class="right">next >></a></div><br><a href="' + link + '"  target="_blank"><img src="' + standard_url + '"></a><br><div class="caption">'+capt+' </div></span>';    
+    lightbox.innerHTML += '<span class="lightbox" id="' + imgId + '"><a href="#_"><div class="close">close x</div></a><div class="prevnext"><a href="#' + prev + '" class="left">&lt;&lt; </a><a href="#' + next + '" class="right"> &gt;&gt;</a></div><br><a href="' + link + '" target="_blank"><img src="' + standardUrl + '"></a><br><div class="caption">' + caption + ' </div></span>';    
 }
