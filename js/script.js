ecf7a5322b984c10972449a7c3e5234d4d9ef06d
Lauren <laurenortencio@gmail.com>
2015-09-25 18:51:01 -0700

    removed some white space

--- js/script.js
+++ js/script.js
@@ -25,12 +25,12 @@ function processInstaData(instagramData) {
         var link = pic.link;
         var imgId = i;
         if (imgId == 0) {
-            prev = NUM_IMAGES - 1;
+            prev = NUM_IMAGES-1;
         }
         else {
             prev = i-1;
         }
-        if (imgId == NUM_IMAGES - 1) {
+        if (imgId == NUM_IMAGES-1) {
             next = 0;
         }
         else {
